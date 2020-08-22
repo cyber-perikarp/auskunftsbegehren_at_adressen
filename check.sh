0e64f122eff1cef5455178016880f4fac33b9e35
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-08-22 17:14:29 +0200

    fix path to csvlint

--- check.sh
+++ check.sh
@@ -21,7 +21,7 @@ for folder in $(ls -d -1 */); do
     echo "file contains ${lines} entries"
 
     # Check the file
-    /tmp/bin/csvlint "${file}"
+    /tmp/go/bin/csvlint "${file}"
 
     # Add number of entries to total sum
     sum=$(echo "${sum}+${lines}" | bc)
