90d7ecf69e29949a168d8a89af7c823e0fe513d1
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-08-22 22:20:38 +0200

    Styling

--- .exporter/exporter.sh
+++ .exporter/exporter.sh
@@ -22,7 +22,7 @@ cat <<EOF >> upload/index.html
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<style type="text/css">body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
+	<style>body{margin:40px auto;max-width:650px;line-height:1.6;font-size:18px;color:#444;padding:0 10px}h1,h2,h3{line-height:1.2}</style>
 	<title>Datensatzdownload</title>
 </head>
 <body>
