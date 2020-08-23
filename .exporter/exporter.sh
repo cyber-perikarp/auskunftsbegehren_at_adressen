6049706afc6c84fa5ce2565f43c0215ce5c08c36
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-08-23 21:45:23 +0200

    General PDF Exporter

--- .exporter/exporter.sh
+++ .exporter/exporter.sh
@@ -28,7 +28,7 @@ figlet "GENERAL HTML"
 mv general.html upload
 
 figlet "GENERAL PDF"
-wkhtmltopdf --page-size A4 --enable-local-file-access upload/general.html upload/general.pdf
+wkhtmltopdf --page-size A4 --enable-local-file-access --footer-left "https://auskunftsbegehren-adressen.cyber-perikarp.eu/" --footer-right "Seite [page] von [topage]" upload/general.html upload/general.pdf
 
 figlet "UPLOAD"
 ls -hall upload
