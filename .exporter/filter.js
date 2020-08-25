e2981c364afbbb37a4a36a0e1d8adfd3deedc9a4
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-08-26 00:19:07 +0200

    HTML Exporter: Filter

--- .exporter/filter.js
+++ .exporter/filter.js
@@ -40,8 +40,8 @@ var btnContainer = document.getElementById("buttonContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("inverse");
+    current[0].className = current[0].className.replace("inverse ", "");
+    this.className += " inverse";
   });
 }
