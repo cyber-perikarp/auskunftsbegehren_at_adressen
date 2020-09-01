538e3ac080f434ed530dd8b3d9a2dc4c79f59def
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-09-01 20:31:31 +0200

    Download page: Show selected items

--- .download/filter.js
+++ .download/filter.js
@@ -10,7 +10,10 @@ $(document).ready(function() {
   var filter = params.get('filter');
 
   // Aktiven Link hervorheben
-  buttonId = '#' + filter + 'Button';
-  $(buttonId).addClass('activeMenuItem');
+  buttonGetter = 'header a:contains(' + filter + ')';
+  $(buttonGetter).addClass('activeMenuItem');
 
+  // Das was wir sehen wollen einblenden
+  activeItemGetter = '#' + filter;
+  $(activeItemGetter).addClass('show');
 });
