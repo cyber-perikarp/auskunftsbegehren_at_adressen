6ff5945f29d2eb8621e73ba250e4d4fd92edb042
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2020-09-02 19:31:16 +0000

    Deploy cyber-perikarp/auskunftsbegehren_at_adressen to github.com/cyber-perikarp/auskunftsbegehren_at_adressen.git:gh-pages

---
+++ prepare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Sebastian Elisa Pfeifer <sebastian@sebastian-elisa-pfeifer.eu>
+
+# Quit on errors
+set -e
+
+# We are in a subfolder
+cd $TRAVIS_BUILD_DIR
+
+mkdir upload
+
+now=$(TZ=Europe/Vienna date +"%A, %d.%m.%Y %T %:z")
+sed -i "s/%%DATE%%/$now/g" .download/index.html
+
+rsync -avP .download/ upload/
