cf1d50657ae7415579abe9b15e5ba3ff595e378e
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-09-02 19:24:20 +0200

    Download page: Use rsync instead of cp

--- .download/prepare.sh
+++ .download/prepare.sh
@@ -7,12 +7,9 @@ set -e
 # We are in a subfolder
 cd $TRAVIS_BUILD_DIR
 
+mkdir upload
+
 now=$(TZ=Europe/Vienna date +"%A, %d.%m.%Y %T %:z")
 sed -i "s/%%DATE%%/$now/g" .download/index.html
-cp .download/index.html upload/index.html
 
-cp .download/style.css upload
-cp .download/jquery-3.5.1.min.js upload
-cp .download/filter.js upload
-cp .download/mini-default.min.css upload
-cp .download/cyber-perikarp-logo.png upload
+rsync -avP .download/ upload/
