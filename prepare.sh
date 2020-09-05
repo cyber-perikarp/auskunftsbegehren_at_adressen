a21780a845877e5366e9591dcaf2e58ba7440f56
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2020-09-05 10:53:20 +0000

    Deploy cyber-perikarp/auskunftsbegehren_at_adressen to github.com/cyber-perikarp/auskunftsbegehren_at_adressen.git:gh-pages

--- prepare.sh
+++ prepare.sh
@@ -8,8 +8,17 @@ set -e
 cd $TRAVIS_BUILD_DIR
 
 mkdir upload
+mkdir qrcodes
 
 now=$(TZ=Europe/Vienna date +"%A, %d.%m.%Y %T %:z")
 sed -i "s/%%DATE%%/$now/g" .download/index.html
 
 rsync -avP .download/ upload/
+
+find upload/js/ -type f \
+    -name "*.js" ! -name "*.min.*" ! -name "vfs_fonts*" \
+    -exec uglifyjs {} --comments -c -m -o {} \;
+
+find upload/css/ -type f \
+    -name "*.css" ! -name "*.min.*" \
+    -exec uglifycss {} --output {} \;
