4d24caa05d2c3d4899f063ff39984b63511d91c7
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2020-09-06 17:05:04 +0200

    CI: Minify HTML

---
+++ .download/minify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Sebastian Elisa Pfeifer <sebastian@sebastian-elisa-pfeifer.eu>
+
+# Quit on errors
+set -e
+
+# We are in a subfolder
+cd $TRAVIS_BUILD_DIR
+
+find upload/js/ -type f \
+    -name "*.js" ! -name "*.min.*" ! -name "vfs_fonts*" \
+    -exec uglifyjs {} --comments -c -m -o {} \;
+
+find upload/css/ -type f \
+    -name "*.css" ! -name "*.min.*" \
+    -exec uglifycss {} --output {} \;
+
+find upload/ -type f \
+    -name "*.html" ! -name "*.html.*" \
+    -exec minify {} --type html -o {} \;
