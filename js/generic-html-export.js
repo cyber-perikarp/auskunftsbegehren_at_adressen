12ec50a11cf34d99df0ec926ee675e1b5fd64be4
Sebastian Elisa Pfeifer (via Travis CI) <sebastian.pfeifer@unicorncloud.org>
2021-01-17 15:04:29 +0000

    Deploy cyber-perikarp/auskunftsbegehren_at_adressen to github.com/cyber-perikarp/auskunftsbegehren_at_adressen.git:gh-pages

--- js/generic-html-export.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(window).width()<1024&&$("#mainNav").addClass("show"),$("#navbar-burger").click(function(){$("#navbar-burger").toggleClass("is-active"),$("#mainNav").toggleClass("show")}),$(".navbar-item").click(function(){$("#mainNav").removeClass("show")})});
